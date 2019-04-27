09e9a6c2574f7af40b485de7f64ba375220e6a7a
Kaito-Dogi <kendobu0405@gmail.com>
2019-04-27 16:25:44 +0900

    init

---
+++ app/src/main/java/com/doggy/memo/Memo.java
@@ -0,0 +1,4 @@
+package com.doggy.memo;
+
+public class Memo {
+}
---
+++ app/src/main/java/com/doggy/memo/MemoApplication.java
@@ -0,0 +1,11 @@
+package com.doggy.memo;
+
+import android.app.Application;
+
+public class MemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
